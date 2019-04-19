9c99c573e1e445fb201876f4ad099f29e4a945dd
steveryan <steveryan6989@gmail.com>
2019-04-19 17:55:10 -0400

    Done! it works

--- cats.rb
+++ cats.rb
@@ -6,12 +6,21 @@ require "pry"
 
 links = []
 
+def make_html(links)
+  html_links = links.map {|link| "<img src=\"" + link + "\" height=\"100\">" + "\n"}
+  contents_of_file = '<!DOCTYPE html>' + "\n" + '<html>' + "\n" + '<head>' + "\n" + '<meta charset="UTF-8">' + "\n" + '<title>Here are some cat pictures</title>' + "\n" + '</head>' + "\n" + '<body>' + "\n" + "#{html_links.join}" + "\n" + '</body>' + "\n" + '</html>'
+  #binding.pry
+  File.write('Cats.html', contents_of_file)
+end
+
 request = RestClient.get "https://api.reddit.com/r/cats"
 parsed_response = JSON.parse(request.body)
 posts = parsed_response["data"]["children"]
 
 posts.each do |json|
-  links << json["data"]["url"]
+  if json["data"]["url"].include?(".jpg")
+    links << json["data"]["url"]
+  end
 end
 
-puts links
+make_html(links)
